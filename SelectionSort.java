7d562567052e9cbbe6d297a798f50a922caab083
Arpit1625 <159800752+Arpit1625@users.noreply.github.com>
2024-10-25 13:35:10 +0530

    Create SelectionSort.java
    Selection sort function in java

---
+++ SelectionSort.java
@@ -0,0 +1,30 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        Sorting sr = new Sorting();
+        int arr[] = {4, 1, 3, 9, 7};
+        int N = 5;
+        sr.selectionSort(arr, N);
+        for(int i=0;  i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
+class Sorting{
+    int  select(int arr[], int i){
+        int index =i;
+       for(int j= i+1; j< arr.length; j++){
+            if(arr[index]>arr[j]){
+                index = j;
+            }
+       }
+       return index;
+	}
+	void selectionSort(int arr[], int n){
+        for(int i=0; i<n-1 ;i++){
+            int ind = select(arr, i);
+            int temp = arr[ind];
+            arr[ind]= arr[i];
+            arr[i] = temp;
+        }
+	}
+}
