a49590b4c4f174bee2979e3b5b899a4943dfa52e
Arpit1625 <159800752+Arpit1625@users.noreply.github.com>
2025-02-01 08:56:28 +0530

    Finding number of occurrence of an element in an array
    we have a given sorted array and we have to find number of occurrence of an element in array that is given in target.

---
+++ NumberOfOccurence.java
@@ -0,0 +1,60 @@
+public class NumberOfOccurence {
+    public static void main(String[] args) {
+        NumberOfOccurence No = new NumberOfOccurence();
+        int arr[] = {1, 1, 2, 2, 2, 2, 3};
+        int tar = 2;
+        int ans = No.countFreq(arr, tar);
+        System.out.println(ans);
+    }
+    int countFreq(int []arr,int target){
+        int[] a = searchRange(arr, target);
+        if(a[0] == -1){
+            return 0;
+        }
+        else{
+            return (a[1] - a[0] +1);
+        }
+    }
+    public int findfirst(int[] nums, int target){
+        int first = -1;
+        int low = 0, high = nums.length - 1;
+        while(low <= high){
+            int mid = (high + low) / 2;
+            if(nums[mid] >= target){
+                first = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return first;
+    }
+    public int findlast(int[] nums, int target){
+        int last = -1;
+        int low = 0, high = nums.length - 1;
+        while(low <= high){
+            int mid = (high + low) / 2;
+            if(nums[mid] <= target){
+                last = mid;
+                low = mid + 1;           
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return last;
+    }
+    public int[] searchRange(int[] nums, int target) {
+        int ans[] = new int[2];
+        int first = findfirst(nums, target);
+        if(nums.length == 0 || first == -1 || nums[first] != target){
+            ans[0] = -1;
+            ans[1] = -1;
+            return ans;
+        }
+        ans[0] = first;
+        ans[1] = findlast(nums, target);
+        return ans;
+    }
+}
