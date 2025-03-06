0a1195f05a57aa26724aa2fdc5a432b4e501ec3c
Arpit1625 <159800752+Arpit1625@users.noreply.github.com>
2025-03-06 19:56:42 +0530

    Search a 2D Matrix II
    Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
    
    Integers in each row are sorted in ascending from left to right.
    Integers in each column are sorted in ascending from top to bottom.

---
+++ TwoDSearch_II.java
@@ -0,0 +1,27 @@
+public class TwoDSearch_II {
+    public static void main(String[] args) {
+        int arr[][] = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        int target = 3;
+        System.out.println(searchMatrix(arr, target));
+    }
+    static boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int row = 0; 
+        int col = cols - 1;
+        while (row < rows && col >= 0) {
+            int current = matrix[row][col];
+            if (current == target) {
+                return true;
+            } else if (current > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+}
