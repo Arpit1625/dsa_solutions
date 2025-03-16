c69a205acfffbbbc509dfb5022f01bdbd9ef13ea
Arpit1625 <159800752+Arpit1625@users.noreply.github.com>
2025-03-16 11:49:39 +0530

    Minimum Jumps
    You are given an array arr[] of non-negative numbers. Each number tells you the maximum number of steps you can jump forward from that position.
    
    For example:
    
    If arr[i] = 3, you can jump to index i + 1, i + 2, or i + 3 from position i.
    If arr[i] = 0, you cannot jump forward from that position.
    Your task is to find the minimum number of jumps needed to move from the first position in the array to the last position.
    
    Note:  Return -1 if you can't reach the end of the array.
    
    Examples :
    
    Input: arr[] = [1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9]
    Output: 3
    Explanation: First jump from 1st element to 2nd element with value 3. From here we jump to 5th element with value 9, and from here we will jump to the last.
    Input: arr = [1, 4, 3, 2, 6, 7]
    Output: 2
    Explanation: First we jump from the 1st to 2nd element and then jump to the last element.
    Input: arr = [0, 10, 20]
    Output: -1
    Explanation: We cannot go anywhere from the 1st element.
    Constraints:
    2 ≤ arr.size() ≤ 104
    0 ≤ arr[i] ≤ 104

---
+++ Problem of the day/MinJump.java
@@ -0,0 +1,43 @@
+public class MinJump{
+    public static void main(String[] args) {
+        int arr[] = {0, 10, 20};
+        System.out.println(minJumps(arr));
+    }
+    static int minJumps(int[] arr) {
+        //    int i = 0; it fails since we can jump from from i to that index but we can also use indexexes between them
+        //    int ans = 0;
+        //    while(i <= arr.length-1){
+        //     if(arr[i] == 0){
+        //         return -1;
+        //     }
+        //     ans++;
+        //     i = i + arr[i]; 
+        //    }
+        //    return ans;
+        int n = arr.length;
+        if (n == 1) {
+            return 0;
+        }
+        if (arr[0] == 0){
+            return -1;
+        }
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            farthest = Math.max(farthest, i + arr[i]);
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+                if (currentEnd >= n - 1) {
+                    return jumps;
+                }
+            }
+            if (i >= farthest) {
+                return -1;
+            }
+        }
+        return -1;
+    }
+}
