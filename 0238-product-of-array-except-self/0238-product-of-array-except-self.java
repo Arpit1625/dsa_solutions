8bceeacd551c9c5d9d673bcf80f471fa174cdb8c
Arpit Pandey <159800752+Arpit1625@users.noreply.github.com>
2026-07-18 13:47:22 +0530

    Time: 2 ms (93.94%), Space: 72.2 MB (28.16%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,29 +1,16 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int arr[] = new int [nums.length];
-        int arr1[] = new int [nums.length];
-        int arr2[] = new int [nums.length];
-        int k=1;
-        arr1[0]=1;
-        arr2[nums.length-1]=1;
-        for(int i=1; i<=nums.length-1; i++){
-            k=k*nums[i-1];
-            arr1[i]=k;
+        int res[]=new int[nums.length];
+        int pref=1;
+        for(int i=0;i<nums.length;i++){
+            res[i]=pref;
+            pref=pref*nums[i];
         }
-        k=1;
-          for(int i= nums.length-2; i>=0; i--){
-            k=k*nums[i+1];
-            arr2[i]=k;
+        int post=1;
+        for(int i=nums.length-1;i>=0;i--){
+            res[i]=post*res[i];
+            post=post*nums[i];
         }
-         for(int i= nums.length-1; i>=0; i--){
-            arr[i]=arr1[i]*arr2[i];
-         }
-          for(int i=0; i< nums.length; i++){
-            System.out.print(arr1[i] + " ");
-          }
-            for(int i=0; i< nums.length; i++){
-            System.out.print("arr[2] "+arr2[i] + " ");
-          }
-         return arr;
+        return res;
     }
 }
